76dcf54645f467935735268ba86527276800cb47
Igor Rodionov <goruha@gmail.com>
2025-02-17 14:19:10 +0100

    Refactor tests

--- test/component_test.go
+++ test/component_test.go
@@ -3,201 +3,146 @@ package test
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/url"
 	"strings"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/service/iam"
 	"github.com/cloudposse/test-helpers/pkg/atmos"
-	helper "github.com/cloudposse/test-helpers/pkg/atmos/aws-component-helper"
+	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/stretchr/testify/assert"
 )
 
 type AssumeRolePolicyDocument struct {
-	Statement []struct {
-		Principal struct {
-			Service string `json:"Service"`
-			Aws     string `json:"AWS"`
-		} `json:"Principal"`
-		Action    []string `json:"Action"`
-		Condition struct {
-			StringEquals    map[string]string   `json:"StringEquals,omitempty"`
-			StringNotEquals map[string][]string `json:"StringNotEquals,omitempty"`
-			Null            map[string]string   `json:"Null,omitempty"`
-			Bool            map[string]bool     `json:"Bool,omitempty"` // Added Bool for new condition
-			ArnLike         map[string][]string `json:"ArnLike,omitempty"`
-			StringLike      map[string][]string `json:"StringLike,omitempty"`
-		} `json:"Condition"`
-	} `json:"Statement"`
+    Statement []struct {
+        Principal struct {
+            Service string `json:"Service"`
+            Aws     string `json:"AWS"`
+        } `json:"Principal"`
+        Action    []string `json:"Action"`
+        Condition struct {
+            StringEquals    map[string]string   `json:"StringEquals,omitempty"`
+            StringNotEquals map[string][]string `json:"StringNotEquals,omitempty"`
+            Null            map[string]string   `json:"Null,omitempty"`
+            Bool            map[string]bool     `json:"Bool,omitempty"` // Added Bool for new condition
+            ArnLike         map[string][]string `json:"ArnLike,omitempty"`
+            StringLike      map[string][]string `json:"StringLike,omitempty"`
+        } `json:"Condition"`
+    } `json:"Statement"`
 }
 
-func TestComponent(t *testing.T) {
-	// Define the AWS region to use for the tests
-	awsRegion := "us-east-2"
+type TeamConfig struct {
+	AllowedRoles          map[string]interface{} `json:"allowed_roles"`
+	AwsSamlLoginEnabled   bool                    `json:"aws_saml_login_enabled"`
+	DeniedPermissionSets   []string                `json:"denied_permission_sets"`
+	DeniedRoleArns        []string                `json:"denied_role_arns"`
+	DeniedTeams           []string                `json:"denied_teams"`
+	MaxSessionDuration    int                     `json:"max_session_duration"`
+	RoleDescription       string                  `json:"role_description"`
+	RolePolicyArns       []string                `json:"role_policy_arns"`
+	TrustedPermissionSets  []string                `json:"trusted_permission_sets"`
+	TrustedRoleArns       []string                `json:"trusted_role_arns"`
+	TrustedTeams          []string                `json:"trusted_teams"`
+}
 
-	// Initialize the test fixture
-	fixture := helper.NewFixture(t, "../", awsRegion, "test/fixtures")
+type ComponentSuite struct {
+	helper.TestSuite
+}
 
-	// Ensure teardown is executed after the test
-	defer fixture.TearDown()
-	fixture.SetUp(&atmos.Options{})
+func (s *ComponentSuite) TestBasic() {
+	const component = "aws-teams/basic"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
 
-	// Define the test suite
-	fixture.Suite("default", func(t *testing.T, suite *helper.Suite) {
-		// Test phase: Validate the functionality of the ALB component
-		suite.Test(t, "basic", func(t *testing.T, atm *helper.Atmos) {
-			inputs := map[string]interface{}{}
-			defer atm.GetAndDestroy("aws-teams/basic", "default-test", inputs)
-			component := atm.GetAndDeploy("aws-teams/basic", "default-test", inputs)
-			assert.NotNil(t, component)
+	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
+	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
+	assert.NotNil(s.T(), options)
 
-			rolesMap := atm.OutputMapOfObjects(component, "team_name_role_arn_map")
-			assert.NotEmpty(t, rolesMap["admin"])
-			assert.NotEmpty(t, rolesMap["viewer"])
+	rolesMap := atmos.OutputMapOfObjects(s.T(), options, "team_name_role_arn_map")
+	assert.NotEmpty(s.T(), rolesMap["admin"])
+	assert.NotEmpty(s.T(), rolesMap["viewer"])
 
-			expectedNames := []string{
-				strings.Split(rolesMap["admin"].(string), "/")[1],
-				strings.Split(rolesMap["viewer"].(string), "/")[1],
-			}
-			names := atm.OutputList(component, "team_names")
-			assert.ElementsMatch(t, expectedNames, names)
+	expectedNames := []string{
+		strings.Split(rolesMap["admin"].(string), "/")[1],
+		strings.Split(rolesMap["viewer"].(string), "/")[1],
+	}
+	names := atmos.OutputList(s.T(), options, "team_names")
+	assert.ElementsMatch(s.T(), expectedNames, names)
 
-			expectedArns := []string{
-				rolesMap["admin"].(string),
-				rolesMap["viewer"].(string),
-			}
-			arns := atm.OutputList(component, "role_arns")
-			assert.ElementsMatch(t, expectedArns, arns)
+	expectedArns := []string{
+		rolesMap["admin"].(string),
+		rolesMap["viewer"].(string),
+	}
+	arns := atmos.OutputList(s.T(), options, "role_arns")
+	assert.ElementsMatch(s.T(), expectedArns, arns)
 
-			config := atm.OutputMapOfObjects(component, "teams_config")
-			adminRoleDescription := config["admin"].(map[string]interface{})["role_description"].(string)
-			viewerRoleDescription := config["viewer"].(map[string]interface{})["role_description"].(string)
 
-			assert.True(t, config["admin"].(map[string]interface{})["aws_saml_login_enabled"].(bool))
-			assert.Equal(t, "viewer", config["admin"].(map[string]interface{})["denied_teams"].([]interface{})[0].(string))
-			assert.Equal(t, float64(43200), config["admin"].(map[string]interface{})["max_session_duration"].(float64))
-			assert.Equal(t, "Team with PowerUserAccess permissions in `identity` and AdministratorAccess to all other accounts except `root`", adminRoleDescription)
-			assert.Equal(t, "arn:aws:iam::aws:policy/PowerUserAccess", config["admin"].(map[string]interface{})["role_policy_arns"].([]interface{})[0].(string))
-			assert.Equal(t, "IdentityAdminTeamAccess", config["admin"].(map[string]interface{})["trusted_permission_sets"].([]interface{})[0].(string))
-			assert.Equal(t, "admin", config["admin"].(map[string]interface{})["trusted_teams"].([]interface{})[0].(string))
+	var config map[string]TeamConfig
+	atmos.OutputStruct(s.T(), options, "teams_config", &config)
+	adminRoleDescription := config["admin"].RoleDescription
+	viewerRoleDescription := config["viewer"].RoleDescription
 
-			assert.False(t, config["viewer"].(map[string]interface{})["aws_saml_login_enabled"].(bool))
-			assert.Equal(t, "viewer", config["viewer"].(map[string]interface{})["denied_teams"].([]interface{})[0].(string))
-			assert.Equal(t, float64(43200), config["viewer"].(map[string]interface{})["max_session_duration"].(float64))
-			assert.Equal(t, "Team restricted to viewing resources in the identity account", viewerRoleDescription)
-			assert.Equal(t, "arn:aws:iam::aws:policy/job-function/ViewOnlyAccess", config["viewer"].(map[string]interface{})["role_policy_arns"].([]interface{})[0].(string))
+	assert.True(s.T(), config["admin"].AwsSamlLoginEnabled)
+	assert.Equal(s.T(), "viewer", config["admin"].DeniedTeams[0])
+	assert.EqualValues(s.T(), 43200, config["admin"].MaxSessionDuration)
+	assert.Equal(s.T(), "Team with PowerUserAccess permissions in `identity` and AdministratorAccess to all other accounts except `root`", adminRoleDescription)
+	assert.Equal(s.T(), "arn:aws:iam::aws:policy/PowerUserAccess", config["admin"].RolePolicyArns[0])
+	assert.Equal(s.T(), "IdentityAdminTeamAccess", config["admin"].TrustedPermissionSets[0])
+	assert.Equal(s.T(), "admin", config["admin"].TrustedTeams[0])
 
-			client := aws.NewIamClient(t, awsRegion)
+	assert.False(s.T(), config["viewer"].AwsSamlLoginEnabled)
+	assert.Equal(s.T(), "viewer", config["viewer"].DeniedTeams[0])
+	assert.EqualValues(s.T(), 43200, config["viewer"].MaxSessionDuration)
+	assert.Equal(s.T(), "Team restricted to viewing resources in the identity account", viewerRoleDescription)
+	assert.Equal(s.T(), "arn:aws:iam::aws:policy/job-function/ViewOnlyAccess", config["viewer"].RolePolicyArns[0])
 
-			adminRoleName := strings.Split(rolesMap["admin"].(string), "/")[1]
+	client := aws.NewIamClient(s.T(), awsRegion)
 
-			describeRoleOutput, err := client.GetRole(context.Background(), &iam.GetRoleInput{
-				RoleName: &adminRoleName,
-			})
-			assert.NoError(t, err)
+	adminRoleName := strings.Split(rolesMap["admin"].(string), "/")[1]
+	viewerRoleName := strings.Split(rolesMap["viewer"].(string), "/")[1]
 
-			awsRole := describeRoleOutput.Role
-			assert.Equal(t, adminRoleName, *awsRole.RoleName)
-			assert.Equal(t, adminRoleDescription, *awsRole.Description)
+	verifyRole(s.T(), client, adminRoleName, "admin")
+	verifyRole(s.T(), client, viewerRoleName, "viewer")
 
-			assert.EqualValues(t, 43200, *awsRole.MaxSessionDuration)
-			assert.Equal(t, "/", *awsRole.Path)
+	s.DriftTest(component, stack, nil)
+}
 
-			assumeRolePolicyDocument, err := url.QueryUnescape(*awsRole.AssumeRolePolicyDocument)
-			assert.NoError(t, err)
+func (s *ComponentSuite) TestEnabledFlag() {
+	const component = "aws-teams/disabled"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
 
-			var assumePolicyDoc AssumeRolePolicyDocument
-			err = json.Unmarshal([]byte(assumeRolePolicyDocument), &assumePolicyDoc)
-			assert.NoError(t, err)
+	s.VerifyEnabledFlag(component, stack, nil)
+}
 
-			assert.Contains(t, assumePolicyDoc.Statement[0].Principal.Aws, "root")
-			assert.ElementsMatch(t, []string{
-				"sts:AssumeRole",
-				"sts:SetSourceIdentity",
-				"sts:TagSession",
-			}, assumePolicyDoc.Statement[0].Action)
+func TestRunSuite(t *testing.T) {
+	suite := new(ComponentSuite)
+	helper.Run(t, suite)
+}
 
-			accountId := aws.GetAccountId(t)
-
-			// Verify assume role conditions
-			assert.NotNil(t, assumePolicyDoc.Statement[0].Condition)
-			assert.Contains(t, assumePolicyDoc.Statement[0].Condition.ArnLike, "aws:PrincipalArn")
-			assert.ElementsMatch(t, []string{
-				fmt.Sprintf("arn:aws:iam::%s:role/tester-admin", accountId),
-				fmt.Sprintf("arn:aws:iam::%s:role/aws-reserved/sso.amazonaws.com*/AWSReservedSSO_IdentityAdminTeamAccess_*", accountId),
-			},
-				assumePolicyDoc.Statement[0].Condition.ArnLike["aws:PrincipalArn"])
-
-			attachedPolicies, err := client.ListAttachedRolePolicies(context.Background(), &iam.ListAttachedRolePoliciesInput{
-				RoleName: &adminRoleName,
-			})
-			assert.NoError(t, err)
-
-			expectedPolicies := []string{
-				"arn:aws:iam::aws:policy/PowerUserAccess",
-			}
-
-			var actualPolicies []string
-			for _, policy := range attachedPolicies.AttachedPolicies {
-				actualPolicies = append(actualPolicies, *policy.PolicyArn)
-			}
-
-			assert.ElementsMatch(t, expectedPolicies, actualPolicies)
-
-			viewerRoleName := strings.Split(rolesMap["viewer"].(string), "/")[1]
-
-			describeRoleOutput, err = client.GetRole(context.Background(), &iam.GetRoleInput{
-				RoleName: &viewerRoleName,
-			})
-			assert.NoError(t, err)
-
-			awsRole = describeRoleOutput.Role
-			assert.Equal(t, viewerRoleName, *awsRole.RoleName)
-			assert.Equal(t, viewerRoleDescription, *awsRole.Description)
-
-			assert.EqualValues(t, 43200, *awsRole.MaxSessionDuration)
-			assert.Equal(t, "/", *awsRole.Path)
-
-			assumeRolePolicyDocument, err = url.QueryUnescape(*awsRole.AssumeRolePolicyDocument)
-			assert.NoError(t, err)
-
-			err = json.Unmarshal([]byte(assumeRolePolicyDocument), &assumePolicyDoc)
-			assert.NoError(t, err)
-
-			assert.Contains(t, assumePolicyDoc.Statement[0].Principal.Aws, "root")
-			assert.ElementsMatch(t, []string{
-				"sts:AssumeRole",
-				"sts:SetSourceIdentity",
-				"sts:TagSession",
-			}, assumePolicyDoc.Statement[0].Action)
-
-			// Verify assume role conditions
-			assert.NotNil(t, assumePolicyDoc.Statement[0].Condition)
-			assert.Contains(t, assumePolicyDoc.Statement[0].Condition.ArnLike, "aws:PrincipalArn")
-			assert.ElementsMatch(t, []string{
-				fmt.Sprintf("arn:aws:iam::%s:role/tester-viewer", accountId),
-				fmt.Sprintf("arn:aws:iam::%s:role/aws-reserved/sso.amazonaws.com*/AWSReservedSSO_IdentityViewerTeamAccess_*", accountId),
-				"arn:aws:iam::*:user/*",
-			},
-				assumePolicyDoc.Statement[0].Condition.ArnLike["aws:PrincipalArn"])
-
-			attachedPolicies, err = client.ListAttachedRolePolicies(context.Background(), &iam.ListAttachedRolePoliciesInput{
-				RoleName: &viewerRoleName,
-			})
-			assert.NoError(t, err)
-
-			expectedPolicies = []string{
-				"arn:aws:iam::aws:policy/job-function/ViewOnlyAccess",
-			}
-			actualPolicies = []string{}
-			for _, policy := range attachedPolicies.AttachedPolicies {
-				actualPolicies = append(actualPolicies, *policy.PolicyArn)
-			}
-
-			assert.ElementsMatch(t, expectedPolicies, actualPolicies)
-
-		})
+func verifyRole(t *testing.T, client *iam.Client, roleName, roleType string) {
+	describeRoleOutput, err := client.GetRole(context.Background(), &iam.GetRoleInput{
+		RoleName: &roleName,
 	})
+	assert.NoError(t, err)
+
+	awsRole := describeRoleOutput.Role
+	assert.Equal(t, roleName, *awsRole.RoleName)
+
+	assumeRolePolicyDocument, err := url.QueryUnescape(*awsRole.AssumeRolePolicyDocument)
+	assert.NoError(t, err)
+
+	var assumePolicyDoc AssumeRolePolicyDocument
+	err = json.Unmarshal([]byte(assumeRolePolicyDocument), &assumePolicyDoc)
+	assert.NoError(t, err)
+
+	assert.Contains(t, assumePolicyDoc.Statement[0].Principal.Aws, "root")
+	assert.ElementsMatch(t, []string{
+		"sts:AssumeRole",
+		"sts:SetSourceIdentity",
+		"sts:TagSession",
+	}, assumePolicyDoc.Statement[0].Action)
+
+	// Additional assertions for role details can be added here
 }
